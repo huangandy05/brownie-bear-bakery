43de5303d601be01059f0fb846952f4efd180d2c
Kevin <kevin.huang6541@gmail.com>
2024-10-18 23:31:08 +1100

    added scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import FAQs from "@/components/FAQs";
 import Hero from "@/components/Hero";
 import HowToOrder from "@/components/HowToOrder";
 import OurSelection from "@/components/OurSelection";
+import AboutSection from "@/components/AboutUs";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen">
       <Hero />
+      <AboutSection />
       <OurSelection />
       <HowToOrder />
       <FAQs />
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from "next/image";
+
+export default function AboutUs() {
+    return (
+        <section className="py-12 md:py-24 bg-gray-100">
+            <div className="container mx-auto px-4">
+                <h2 className="text-4xl sm:text-5xl text-center mb-8">ABOUT US</h2>
+                <div className="flex flex-col md:flex-row items-center gap-8">
+                    <div className="relative w-5/6 max-w-sm aspect-square mx-auto md:mx-0">
+                        <Image
+                            src="/hero_section_image.svg"
+                            alt="Hero Image"
+                            fill
+                            className="object-cover outline outline-[10px] outline-yellow-950 rounded-full"
+                        />
+                    </div>
+                    <div className="text-center md:text-left">
+                        <p className="text-lg leading-relaxed text-gray-700">
+                            Brownie Bear Bakery started with a simple idea: sharing a sweet treat with friends. It all began with my signature Mochi Brownies, which quickly became a hit at parties and gatherings. After seeing how much everyone loved them, I thought, why not share these delicious treats with more people?
+                            I ensure to use only the best ingredients and bake everything fresh to order. Whether you’re a fan of classic fudgy brownies or adventurous flavours like Biscoff, there’s something for everyone.
+                            Order today!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
