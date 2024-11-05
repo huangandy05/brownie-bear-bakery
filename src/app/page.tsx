c706800ff193f5f583f1a63ce80f0c90b115377e
Kevin <kevin.huang6541@gmail.com>
2024-11-05 21:16:02 +1100

    removed about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import FAQs from "@/components/FAQs";
 import Hero from "@/components/Hero";
 import HowToOrder from "@/components/HowToOrder";
 import OurSelection from "@/components/OurSelection";
-import AboutSection from "@/components/AboutUs";
 import UnavailabilityBanner from "@/components/AwayBanner";
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
     <main className="flex flex-col items-center min-h-screen">
       <UnavailabilityBanner />
       <Hero />
-      <AboutSection />
       <OurSelection />
       <HowToOrder />
       <FAQs />
