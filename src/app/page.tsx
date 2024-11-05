b1d65ed1ddfc59a3399aff17fe798e53bb5cfd67
Kevin <kevin.huang6541@gmail.com>
2024-11-05 21:15:14 +1100

    added away banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Hero from "@/components/Hero";
 import HowToOrder from "@/components/HowToOrder";
 import OurSelection from "@/components/OurSelection";
 import AboutSection from "@/components/AboutUs";
+import UnavailabilityBanner from "@/components/AwayBanner";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen">
+      <UnavailabilityBanner />
       <Hero />
       <AboutSection />
       <OurSelection />
